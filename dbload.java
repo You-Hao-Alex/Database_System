1ecf1dbcbf327f9ceb4bd00a3ba3babd9838b058
rmit-s3583715-you-hao <s3583715@student.rmit.edu.au>
2018-03-22 16:42:30 +1100

    Add files via upload

--- dbload.java
+++ dbload.java
@@ -5,21 +5,18 @@ import java.io.FileReader;
 
 public class dbload {
 	public static void main(String[] args) {
-		BufferedReader file;
+		String last;
 		try {
-			file = new BufferedReader(new FileReader("BUSINESS_NAMES_201803.csv"));
+			BufferedReader file = new BufferedReader(new FileReader("BUSINESS_NAMES_201803.csv"));
 			String line = null;
 			while ((line = file.readLine()) != null) {
 				String item[] = line.split("/t");
-				String last = item[item.length-1];
+				last = item[item.length-1];
 				System.out.println(last);
 			}
-			
 		}catch (Exception e) {
-			System.out.println("Sorry, file can't be read.");
+			System.err.println("Sorry, file can't be read.");
 		}
 		// load the csv data into 'file'
-		
-		
 	}
 }
