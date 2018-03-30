c0972f39b827cd12d85736a616570cc0eec6c398
rmit-s3583715-you-hao <s3583715@student.rmit.edu.au>
2018-03-30 14:51:40 +1100

    Add files via upload
    complete the structure of the dbquery function.

--- dbquery.java
+++ dbquery.java
@@ -1,7 +1,27 @@
 package Read;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class dbquery {
 	public static void main(String[] args) {
+		try {
+			String text = args[0];
+			int pagesize = Integer.parseInt(args[1]);
+			String datafile = "heap." + args[1];
+			dbquery a = new dbquery();
+			a.search(text, pagesize, datafile);
+		} catch (Exception e) {
+			System.err.println("Can't get the input");
+		}
+	}
+
+	private void search(String text, int pagesize, String datafile) {
+		try {
+			FileInputStream heap = new FileInputStream(new File(datafile));
+		} catch (Exception e) {
+			System.err.println("Can't find a result");
+		}
 
 	}
 }
