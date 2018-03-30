fc9b56413c57be89af85b79eb14b9a3d237d5bb0
rmit-s3583715-you-hao <s3583715@student.rmit.edu.au>
2018-03-31 03:42:09 +1100

    Add files via upload
    Complete all the functions and hope to get a good mark!

--- Searching.java
+++ Searching.java
@@ -2,102 +2,100 @@ package Read;
 
 import java.util.ArrayList;
 
-public class Searching {
+/**
+ * @Date 31/03/2018
+ * @author You Hao s3583715
+ * @Description: Searching class. Most functions are operated in this class. Also converting functions
+ * @Version 1.0
+ */
 
+public class Searching {
 	public static ArrayList<String[]> getRecord(String text, byte[] page) {
 		byte[] index = new byte[2]; // Create an array to save 2 bytes
-
 		System.arraycopy(page, 0, index, 0, 2); // Copy the first 2 bytes into index[]
 		int recordno = Searching.byteToShort(index); // Convert these 2 bytes into short and save it as an integer
-
-		int[] position = new int[(recordno)];
-		int[] length = new int[recordno];
-		for (int i = 0; i < recordno; i++) {
+		int[] position = new int[(recordno)]; // Store the starting position of records
+		int[] length = new int[recordno]; // Store the length of records
+		for (int i = 0; i < recordno; i++) { // Fill in the data of record position
 			System.arraycopy(page, 2 * (i + 1), index, 0, 2);
 			position[i] = Searching.byteToShort(index);
 			// System.out.println(position[i]);
 		}
-		for (int i = 0; i < (recordno - 1); i++) {
+		for (int i = 0; i < (recordno - 1); i++) { // Fill in the data of record length
 			length[i] = position[i + 1] - position[i];
 			// System.out.println(length[i]);
 		}
 		System.arraycopy(page, (position[(recordno - 2)] + length[(recordno - 2)] + 18), index, 0, 2);
-		length[(recordno - 1)] = Searching.byteToShort(index);
+		length[(recordno - 1)] = Searching.byteToShort(index); // The length of the last record in this page
 		// System.out.println(length[(recordno-1)]);
 		ArrayList<Integer> resultIndex = Searching.getField(page, position, length, text);
+		// Get positions of target records
 		ArrayList<String[]> result = Searching.getResult(page, position, length, resultIndex);
-		for(int i = 0; i<result.size();i++) {
-			System.out.println(result.get(i)[0]);
-			System.out.println(result.get(i)[1]);
-		}
 		return result;
 	}
 
-	private static ArrayList<String[]> getResult(byte[] page, int[] position, int[] length, ArrayList<Integer> resultIndex) {
-		
+	// Get the result and store them in an array list
+	private static ArrayList<String[]> getResult(byte[] page, int[] position, int[] length,
+			ArrayList<Integer> resultIndex) {
 		ArrayList<String[]> getResult = new ArrayList<String[]>();
-		String[] a = new String[2];
-		
+		String[] a = new String[2]; // To store the items in each field
 		for (int i = 0; i < resultIndex.size(); i++) {
-			byte[] index = new byte[(length[resultIndex.get(i)]-8-20)];
-			System.arraycopy(page, position[resultIndex.get(i)]+20, index, 0, (length[resultIndex.get(i)]-8-20));
-			String Eight = new String(index);
+			byte[] index = new byte[(length[resultIndex.get(i)] - 8 - 20)];
+			System.arraycopy(page, position[resultIndex.get(i)] + 20, index, 0, (length[resultIndex.get(i)] - 8 - 20));
+			// Get the copy of the first 8 fields in this record
+			String Eight = new String(index); // Store the first 8 fields' contents
 			byte[] nine = new byte[8];
-			System.arraycopy(page, (position[resultIndex.get(i)+1]-8+20), nine, 0, 8);
+			System.arraycopy(page, (position[resultIndex.get(i)] + length[resultIndex.get(i)] - 8), nine, 0, 8);
+			// Get the 9th field which is in Long
 			long Nine = Searching.byteToLong(nine);
-			System.out.println(Nine+"abc");
-			String Ninely = ""+Nine;
+			String Ninely = "" + Nine; // Convert into String
 			a[0] = Eight;
 			a[1] = Ninely;
+			System.out.println(a[0] + "" + a[1]); // Print the result
 			getResult.add(a);
-			
 		}
-
 		return getResult;
 	}
 
+	// Get the position of targeting records
 	private static ArrayList<Integer> getField(byte[] page, int[] position, int[] length, String text) {
 		ArrayList<Integer> Target = new ArrayList<Integer>();
-		for (int i = 0; i < 1; i++) {
-			int[] fieldPosition = new int[10];
-			int[] fieldLength = new int[9];
+		for (int i = 0; i < position.length; i++) {
+			int[] fieldPosition = new int[10]; // Store field position
+			int[] fieldLength = new int[9]; // Store field length
 			for (int j = 0; j < 10; j++) {
-				byte[] index = new byte[2];
+				byte[] index = new byte[2]; // Store index
 				System.arraycopy(page, (position[i] + 2 * j), index, 0, 2);
-				fieldPosition[j] = Searching.byteToShort(index);
+				fieldPosition[j] = Searching.byteToShort(index); // Fill in field position
 				// System.out.println(fieldPosition[j]);
 			}
 			for (int j = 0; j < 9; j++) {
-				fieldLength[j] = fieldPosition[j + 1] - fieldPosition[j];
+				fieldLength[j] = fieldPosition[j + 1] - fieldPosition[j]; // Fill in field length
 				// System.out.println(fieldLength[j]);
 			}
-			byte[] BN = new byte[fieldLength[1]];
+			byte[] BN = new byte[fieldLength[1]]; // Find the info in the second field
 			System.arraycopy(page, position[i] + fieldPosition[1], BN, 0, fieldLength[1]);
-			String BNname = new String(BN);
-
+			String BNname = new String(BN); // Get the BN name
 			if (BNname.contains(text))
-				Target.add(i);
-		}
-
-		for (int j = 0; j < Target.size(); j++) {
-			System.out.println(Target.get(j));
+				Target.add(i); // Add the index to target ArrayList
 		}
 		return Target;
 	}
 
-	public static short byteToShort(byte[] b) {
+	// Convert byte[] to short
+	public static short byteToShort(byte[] index) {
 		short result;
-		result = (short) (b[0] << 8 | b[1] & 0xff);
+		result = (short) (index[0] << 8 | index[1] & 0xff);
 		return result;
 	}
 
-	public static long byteToLong(byte[] b) {
+	// Convert byte[] to Long
+	public static long byteToLong(byte[] index) {
 		long result = 0;
 		for (int i = 0; i < 8; i++) {
 			result <<= 8;
-			result |= (b[i] & 0xff);
+			result |= (index[i] & 0xff);
 		}
 		return result;
 	}
-
 }
--- dbquery.java
+++ dbquery.java
@@ -3,32 +3,47 @@ package Read;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * @Date 31/03/2018
+ * @author You Hao s3583715
+ * @Description: dbquery main class. Search for a query in a heap file and print the results
+ * @Version 1.0
+ */
 
 public class dbquery {
 	public static void main(String[] args) {
 		try {
-			String text = args[0];
-			int pagesize = Integer.parseInt(args[1]);
-			String datafile = "heap." + args[1];
+			Date start = new Date(); // Mark the starting time
+			String text = args[0]; // Get the text query
+			int pagesize = Integer.parseInt(args[1]); // Get the pagesize
+			String datafile = "heap." + args[1]; // Get the file name
 			dbquery input = new dbquery();
-			input.search(text, pagesize, datafile);
+			input.search(text, pagesize, datafile); // Do the operations
+			Date finish = new Date(); // Mark the ending time
+			System.out.println("Time to do search operations in milliseconds is " + (finish.getTime() - start.getTime()));
+			// Compare to get the running time
 		} catch (Exception e) {
 			System.err.println("Can't get the input");
 		}
 	}
 
+	// To print the result and implement the operations
 	private void search(String text, int pagesize, String datafile) {
 		try {
 			// System.out.println(text+""+pagesize+""+datafile); //testing
 			FileInputStream heap = new FileInputStream(new File(datafile));
 			byte[] page = new byte[pagesize]; // Get the data page by page
 			int pageNo = 0; // Set the searching index
-			heap.read(page, pageNo, pagesize); // Read from heap file and get the first page into page array
-			ArrayList<String[]> Result = Searching.getRecord(text, page);
-
+			// Read from heap file and get the first page into page array
+			while ((heap.read(page, pageNo, pagesize)) != -1) {
+				ArrayList<String[]> Result = Searching.getRecord(text, page);
+				if (Result.size() == 0)
+					System.out.println("No matching results in current page");
+			}
 		} catch (Exception e) {
-			System.err.println("Can't find a result");
+			System.err.println("Incorrect input");
 		}
 	}
-
 }
