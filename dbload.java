782c71bc6f54c2aebb7ba836b0a7a0ea6a1ccda3
rmit-s3583715-you-hao <s3583715@student.rmit.edu.au>
2018-03-31 22:36:33 +1100

    Add files via upload
    Modify for AWS environment

--- dbload.java
+++ dbload.java
@@ -15,8 +15,8 @@ import java.util.Date;
 
 public class dbload {
 	public static void main(String[] args) {
-		int pagesize; // The input pagesize
-		String datafile; // The input filename
+		int pagesize = 0; // The input pagesize
+		String datafile = null; // The input filename
 		int recordNo = 0; // Count the number of records loaded
 		int pageNo = 0; // Count the number of pages used
 		Long createTime = null; // Count the number of milliseconds to create heap file
@@ -30,9 +30,14 @@ public class dbload {
 				pagesize = Integer.parseInt(args[2]);
 				datafile = args[0];
 			}
+		} catch (Exception e) {
+			System.out.println("Invalid input");
+		}
+		Writing heap = new Writing("heap." + pagesize); // To transfer into heap file
+		try {
+			Date start = new Date(); // Mark the starting time
 			BufferedReader file = new BufferedReader(new FileReader(datafile));
 			String line = null; // Use line to store the content in this line
-			ArrayList<Pages> pages = new ArrayList<Pages>(); // Use ArrayList to save records
 			Pages page = new Pages(pagesize); // Start with the first page
 			file.readLine();
 			while ((line = file.readLine()) != null) {
@@ -46,7 +51,6 @@ public class dbload {
 					field[fieldIndex] = new Fields(item[fieldIndex], fieldIndex);
 					fieldIndex++;
 				}
-
 				Records record = new Records(field); // Create record to save lengths and fields (one tier)
 				recordNo++;
 				if ((page.getCurrent() + record.getLength() + 2) <= pagesize) {
@@ -54,44 +58,12 @@ public class dbload {
 					page.addRecord(record);
 				} else {
 					// Otherwise, Store it in a new page
-					/**
-					 * if (pages.size() == 2) // For testing 
-					 * break;
-					 **/
-					pages.add(page);
+
+					write(page, pagesize, heap);
 					page = new Pages(pagesize);
 					pageNo++;
 					page.addRecord(record);
 				}
-			}
-			Date start = new Date(); // Mark the starting time
-			Writing heap = new Writing("heap." + pagesize); // To transfer into heap file
-			for (int i = 0; i < pages.size(); i++) {
-				heap.writeShort((short) pages.get(i).getRecord().size()); // Store the number of records for this page
-				int index = 2 + pages.get(i).getRecord().size() * 2; // The starting index of first record
-																		// 2 for total record number
-
-				for (int n = 0; n < pages.get(i).getRecord().size(); n++) { // Write the record index
-					heap.writeShort((short) index); // The start index for the current record
-					index += pages.get(i).getRecord().get(n).getLength(); // The start index for next record
-				}
-
-				for (int n = 0; n < pages.get(i).getRecord().size(); n++) {
-					int indexRecord = 20; // 2*9 to store the index of 9 fields in a single record
-					for (int l = 0; l < 9; l++) { // Write the field index
-						heap.writeShort((short) indexRecord); // Write the index for each field
-						// System.out.println(indexRecord);
-						indexRecord += pages.get(i).getRecord().get(n).getRecord()[l].getLength();
-					}
-					heap.writeShort((short) indexRecord); // Write an ending index
-
-					for (int m = 0; m < 9; m++) { // Write the field
-						heap.write(pages.get(i).getRecord().get(n).getRecord()[m]); // Write BI for a field
-					}
-				}
-				int restCapacity = pages.get(i).getRest(); // Get the rest capacity of the page
-				byte rest[] = new byte[restCapacity];
-				heap.writeBinary(rest); // Fill in " " to achieve page size
 			}
 			Date finish = new Date(); // Mark the ending time
 			createTime = finish.getTime() - start.getTime(); // Compare to get the running time
@@ -102,5 +74,30 @@ public class dbload {
 		System.out.println("The number of records loaded is " + recordNo);
 		System.out.println("The number of pages used is " + pageNo);
 		System.out.println("The number of milliseconds to create the heap file is " + createTime);
+	}
+
+	public static void write(Pages page, int pagesize, Writing heap) {
+		heap.writeShort((short) page.getRecord().size()); // Store the number of records for this page
+		int index = 2 + page.getRecord().size() * 2; // The starting index of first record
+														// 2 for total record number
+		for (int n = 0; n < page.getRecord().size(); n++) { // Write the record index
+			heap.writeShort((short) index); // The start index for the current record
+			index += page.getRecord().get(n).getLength(); // The start index for next record
+		}
+		for (int n = 0; n < page.getRecord().size(); n++) {
+			int indexRecord = 20; // 2*9 to store the index of 9 fields in a single record
+			for (int l = 0; l < 9; l++) { // Write the field index
+				heap.writeShort((short) indexRecord); // Write the index for each field
+				// System.out.println(indexRecord);
+				indexRecord += page.getRecord().get(n).getRecord()[l].getLength();
+			}
+			heap.writeShort((short) indexRecord); // Write an ending index
+			for (int m = 0; m < 9; m++) { // Write the field
+				heap.write(page.getRecord().get(n).getRecord()[m]); // Write BI for a field
+			}
+		}
+		int restCapacity = page.getRest(); // Get the rest capacity of the page
+		byte rest[] = new byte[restCapacity];
+		heap.writeBinary(rest); // Fill in " " to achieve page size
 	}
 }
